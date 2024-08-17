d437487211653ee94cc4b5e329cf3443cc74f296
Semyon Zabolotko <semenzabolotko.ib@gmail.com>
2024-08-17 20:33:48 +0500

    Сделал защиту чувствительных данных(теперь они достаются через конфигу запуска)

--- config/configs.py
+++ config/configs.py
@@ -1,9 +1,10 @@
-host = 'localhost'
-user = 'postgres'
-password='1234'
-db_name='cyberhelpbot'
-port= 5432
+import os
 
-token = '6917064050:AAH11UnUNTaC47hBb9VjwRjm5wLZPtUD5H8'
-api_key = 'at_OIfJXCQ8B0eHMarhOubkBIosKovp2'
-# api_key = 'at_y3Z1FMIM82C6cZreSgp7NQjhEa6Q4'
+host = os.environ.get("DB_HOST")
+user = os.environ.get("DB_USER")
+password = os.environ.get("DB_PASSWORD")
+db_name = os.environ.get("DB_NAME")
+port = os.environ.get("DB_PORT")
+
+token = os.environ.get("BOT_TOKEN")
+api_key = os.environ.get("API_KEY")
